c46b79bdd6250e7a452abd1dfbe843794a97fd5e
YASH-ROCKSTAR <yash.nigam_cs19@gla.ac.in>
2022-01-07 23:55:51 +0530

    objects done

---
+++ ManipulatingWebsite.js
@@ -0,0 +1,17 @@
+console.log('We are manpulating website using js object');
+//window is global object
+// var a=window;
+var b=window.document;
+// alert('hello Yash');
+// a=prompt('This will destroy.Type your name');
+// a=confirm('Are you sure to delete?')
+// console.log(a);
+b=window.innerHeight;
+b=window.innerWidth;
+b=scrollX;
+b=scrollY;
+b=location
+b=location.toString();
+b=window.history;
+console.log(b);
+ 
