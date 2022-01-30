0a17b608144b2155fdc3c92dc8dd8f3f4956416d
YASH-ROCKSTAR <yash.nigam_cs19@gla.ac.in>
2022-01-30 11:12:06 +0530

    MATH fucntions done

---
+++ Mathobject.js
@@ -0,0 +1,32 @@
+console.log("welcome to math object");
+let x=3;
+let y=6;
+let z;
+z=x-y;
+z=x+y;
+z=x*y;
+z=x/y;
+console.log(z);
+z=Math;
+z=Math.PI;
+z=Math.E;
+// z=Math.round(5.50);
+// z=Math.ceil(5.3); //uppar jaye gya
+// z=Math.floor(5.3)
+z=Math.floor(-5.3);
+z=Math.abs(-5);
+z=Math.sqrt(49);
+z=Math.pow(2,6);
+z=Math.min(2,3,8);
+z=Math.max(2,3,8);
+//generating random no
+z=Math.random();
+z=100*Math.random()
+z=50+(100-50)*Math.random()
+///we want in integer
+z=Math.ceil(50+(100-50)*Math.random());
+
+// a=(0,1);
+// a100=a*100=(0,100);
+// a10_100=10+a-(100-10);
+console.log(z);
